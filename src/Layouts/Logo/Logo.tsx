c0ea1ef5ce46435c4ef85247ad6423dbaf785e2a
Ryczko <konradryczko1@gmail.com>
2022-03-18 20:10:54 +0100

    Fix navigation view

--- src/Layouts/Logo/Logo.tsx
+++ src/Layouts/Logo/Logo.tsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 import "./Logo.scss";
 
 function Logo() {
-  return (
-    <Link to={"/"} className="flex items-center">
-      <div className="heart mr-4"></div>
-      Employee Pulse
-    </Link>
-  );
+    return (
+        <Link to={"/"} className="flex items-center text-gray-600">
+            <div className="heart mr-4"></div>
+            Employee Pulse
+        </Link>
+    );
 }
 
 export default Logo;
--- src/Layouts/Navigation/Navigation.tsx
+++ src/Layouts/Navigation/Navigation.tsx
@@ -1,13 +1,13 @@
 import Logo from "../Logo/Logo";
 
 function Navigation() {
-  return (
-    <nav className="py-5">
-      <div className="container m-auto flex px-4 md:px-8">
-        <Logo />
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="py-5 border-b" style={{ borderColor: "#E5E5E5" }}>
+            <div className="container m-auto flex px-4 md:px-8">
+                <Logo />
+            </div>
+        </nav>
+    );
 }
 
 export default Navigation;
