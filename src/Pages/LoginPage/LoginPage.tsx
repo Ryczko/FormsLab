df4e2e3ab1f6b52425229d2fd4d00a2be6713dbc
Paweł Kałdus <xdk78888@gmail.com>
2022-03-24 17:44:00 +0100

    wip: add auth via firebase

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,17 +1,59 @@
+import { useEffect } from 'react';
 import Github from '../../Assets/github.svg';
 import Google from '../../Assets/google.svg';
-import Microsoft from '../../Assets/microsoft.svg';
 import LoginButton from '../../Components/LoginButton/LoginButton';
+import {
+  auth,
+  signInWithGoogle,
+  signInWithGithub,
+  logout,
+} from '../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Button, { ButtonVariant } from '../../Components/Button';
 
+// todo: clean up code and add other providers
 function LoginPage() {
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      console.log('user', user);
+    }
+  }, [user, loading]);
+
   return (
     <div className="container px-4 m-auto mt-10 text-center md:px-8">
       <h1 className="text-6xl font-bold text-center">Sign In</h1>
       <div className="flex flex-col items-center justify-center pt-16">
-        <LoginButton image={Google} href="https://www.google.com/" loginType="Sign in with Google" />
-        <LoginButton image={Microsoft} href="https://www.microsoft.com/" loginType="Sign in with Microsoft" />
-        <LoginButton image={Github} href="https://www.github.com/" loginType="Sign in with Github" />
+        <LoginButton
+          image={Google}
+          loginType="Sign in with Google"
+          onClick={signInWithGoogle}
+        />
+        <LoginButton
+          image={Github}
+          loginType="Sign in with Github"
+          onClick={signInWithGithub}
+        />
       </div>
+      {loading && (
+        <div className="text-center text-zinc-600 text-sm">Loading...</div>
+      )}
+      {error && (
+        <div className="text-center text-red-600 text-sm">
+          Error: {error.message}
+        </div>
+      )}
+      {user && (
+        <div className="m-4">
+          <div className="text-center text-zinc-600 text-sm">
+            Logged in as {user.displayName} ({user.email})
+          </div>
+          <Button variant={ButtonVariant.DANGER} onClick={logout}>
+            Logout
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
