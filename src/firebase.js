d1b2aba2df441c17ff4bad483245b10334907fc0
Ryczko <konradryczko1@gmail.com>
2022-04-10 12:19:35 +0200

    Change user creating way

--- src/firebase.js
+++ src/firebase.js
@@ -11,11 +11,9 @@ import {
 } from 'firebase/auth';
 import {
   getFirestore,
-  query,
-  getDocs,
-  collection,
-  where,
-  addDoc,
+  getDoc,
+  setDoc,
+  doc,
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -39,10 +37,9 @@ const signInWithGoogle = async () => {
   try {
     const res = await signInWithPopup(auth, googleProvider);
     const user = res.user;
-    const q = query(collection(db, 'users'), where('uid', '==', user.uid));
-    const docs = await getDocs(q);
-    if (docs.docs.length === 0) {
-      await addDoc(collection(db, 'users'), {
+    const docs = await getDoc(doc(db, 'users', user.uid));
+    if (!docs.exists()) {
+      await setDoc(doc(db, 'users', user.uid), {
         uid: user.uid,
         name: user.displayName,
         authProvider: 'google',
@@ -58,10 +55,10 @@ const signInWithGithub = async () => {
   try {
     const res = await signInWithPopup(auth, githubProvider);
     const user = res.user;
-    const q = query(collection(db, 'users'), where('uid', '==', user.uid));
-    const docs = await getDocs(q);
-    if (docs.docs.length === 0) {
-      await addDoc(collection(db, 'users'), {
+    const docs = await getDoc(doc(db, 'users', user.uid));
+
+    if (!docs.exists()) {
+      await setDoc(doc(db, 'users', user.uid), {
         uid: user.uid,
         name: user.displayName,
         authProvider: 'github',
@@ -85,7 +82,7 @@ const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
-    await addDoc(collection(db, 'users'), {
+    await setDoc(doc(db, 'users', user.uid), {
       uid: user.uid,
       name,
       authProvider: 'local',
