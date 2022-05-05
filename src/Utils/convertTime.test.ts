1c2261e84e0d3b68d2cbcc2e68ad587506df62d8
Paweł Kałdus <xdk78888@gmail.com>
2022-05-05 19:00:21 +0200

    add missing tests

---
+++ src/Utils/convertTime.test.ts
@@ -0,0 +1,21 @@
+import { Timestamp } from 'firebase/firestore';
+import {
+  formatFirebaseDateWithHours,
+  formatFirebaseDateWithoutHours,
+} from './convertTime';
+
+describe('convertTime', () => {
+  it('should return correct date with hours', () => {
+    const timestamp: Timestamp = new Timestamp(1651768396, 0);
+    const expected = '5.05.2022, 18:33';
+    const result = formatFirebaseDateWithHours(timestamp);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return correct date without hours', () => {
+    const timestamp: Timestamp = new Timestamp(1651768396, 0);
+    const expected = '5.05.2022';
+    const result = formatFirebaseDateWithoutHours(timestamp);
+    expect(result).toEqual(expected);
+  });
+});
