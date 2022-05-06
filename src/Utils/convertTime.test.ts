9176a3afab55ba22a03e96a4c6d7da813213df05
xdk78 <xdk78888@gmail.com>
2022-05-06 13:30:37 +0200

    try to fix ci env

--- src/Utils/convertTime.test.ts
+++ src/Utils/convertTime.test.ts
@@ -12,9 +12,9 @@ describe('convertTime', () => {
     // fix for different timezones on CI
     if (process.env.CI) {
       expected = '5.05.2022, 16:33';
+    } else {
+      expected = '5.05.2022, 18:33';
     }
-    expected = '5.05.2022, 18:33';
-
     const result = formatFirebaseDateWithHours(timestamp);
     expect(result).toEqual(expected);
   });
