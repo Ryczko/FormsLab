8d16721d73fec8c60704cfe97d1ec8105ae6be02
xdk78 <xdk78888@gmail.com>
2022-05-06 13:22:08 +0200

    try to fix ci env

--- src/Utils/convertTime.test.ts
+++ src/Utils/convertTime.test.ts
@@ -8,11 +8,9 @@ describe('convertTime', () => {
   it('should return correct date with hours', () => {
     const timestamp: Timestamp = new Timestamp(1651768396, 0);
     let expected: string;
-    
+
     // fix for different timezones on CI
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    if (process.env.NODE_ENV === 'ci') {
+    if (process.env.CI) {
       expected = '5.05.2022, 16:33';
     }
     expected = '5.05.2022, 18:33';
