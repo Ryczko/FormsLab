95ea1e02af62c759535812e5c6a67be74a3bcf22
Paweł Kałdus <xdk78888@gmail.com>
2022-05-07 15:48:01 +0200

    improve page animation

--- src/HOC/withAnimation.tsx
+++ src/HOC/withAnimation.tsx
@@ -3,10 +3,10 @@ import { motion } from 'framer-motion';
 export default (WrappedComponent) => {
   const hocComponent = ({ ...props }) => (
     <motion.div
-      initial={{ opacity: 0, x: '50%' }}
-      animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: '-50%' }}
-      transition={{ duration: 0.3, type: 'tween' }}
+      initial={{ opacity: 0, y: 16 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -16 }}
+      transition={{ duration: 0.15 }}
     >
       <WrappedComponent {...props} />
     </motion.div>
