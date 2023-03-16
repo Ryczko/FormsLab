ba104e4adfc5c1f832dbe162f80b55aad386bd30
Piotr Matyjasik <matyjasik.piotr@wp.pl>
2023-03-16 10:31:45 +0100

    Feature/404 page (#73)

---
+++ src/pages/404.tsx
@@ -0,0 +1,24 @@
+import useRedirectWithTimeout from 'src/shared/hooks/useRedirectWithTimeout';
+import Head from 'next/head';
+
+export default function FourOhFour() {
+  const { secondsRemaining } = useRedirectWithTimeout('/', 5);
+  return (
+    <>
+      <Head>
+        <title>Page not found</title>
+        <meta
+          name="description"
+          content="Employee Pulse (404) - This page cannot be found."
+        />
+      </Head>
+      <h1 className="text-2xl font-bold text-center">
+        This page cannot be found.
+      </h1>
+      <h2 className="mt-4 text-xl text-center">
+        Redirecting to Homepage in {secondsRemaining}
+        {secondsRemaining > 1 ? ' seconds' : ' second'}.
+      </h2>
+    </>
+  );
+}
