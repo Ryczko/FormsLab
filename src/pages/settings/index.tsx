4ff5f2f2b5c2ac59ab42ee639fa0b15fc25de4ae
xdk78 <xdk78888@gmail.com>
2023-03-18 17:43:12 +0100

    Make Dialog more reusable

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,15 +1,14 @@
 import withAnimation from '../../shared/HOC/withAnimation';
 import Head from 'next/head';
 import Header from 'src/shared/components/Header/Header';
-import { Fragment } from 'react';
 import withProtectedRoute from 'src/shared/HOC/withProtectedRoute';
 import IconButton, {
   IconButtonVariant,
 } from 'src/shared/components/IconButton/IconButton';
 import { TrashIcon } from '@heroicons/react/outline';
-import { Dialog, Transition } from '@headlessui/react';
 import Button, { ButtonVariant } from 'src/shared/components/Button/Button';
 import { useSettingsManager } from 'src/features/settings/settingsManager';
+import StyledDialog from 'src/shared/components/StyledDialog/StyledDialog';
 
 function SettingsPage() {
   const {
@@ -30,73 +29,38 @@ function SettingsPage() {
       <div className="container px-4 m-auto text-center md:px-8">
         <Header>Hi {user.displayName}!</Header>
         <div className="flex flex-col justify-center items-center space-y-2">
-          <Transition appear show={isOpen} as={Fragment}>
-            <Dialog
-              as="div"
-              className="relative z-10"
-              onClose={closeDeleteModal}
-            >
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="fixed inset-0 bg-zinc-900 bg-opacity-25" />
-              </Transition.Child>
-
-              <div className="overflow-y-auto fixed inset-0">
-                <div className="flex justify-center items-center p-4 min-h-full text-center">
-                  <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                  >
-                    <Dialog.Panel className="flex overflow-hidden flex-col justify-center p-6 w-auto max-w-md text-left bg-white rounded-md shadow-xl transition-all">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-lg font-medium leading-6 text-zinc-900"
-                      >
-                        Delete my account
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <p className="text-sm text-red-500">
-                          Are you sure you want to&nbsp;
-                          <span className="font-bold">delete</span> your
-                          account?
-                        </p>
-                      </div>
-
-                      <div className="flex justify-between mt-6 space-x-3">
-                        <Button
-                          variant={ButtonVariant.SECONDARY}
-                          onClick={closeDeleteModal}
-                          className="uppercase"
-                        >
-                          Cancel
-                        </Button>
-                        <IconButton
-                          variant={IconButtonVariant.DANGER}
-                          onClick={handleOnAccountDelete}
-                          icon={<TrashIcon className="w-5 h-5" />}
-                          className="uppercase"
-                        >
-                          Delete my account
-                        </IconButton>
-                      </div>
-                    </Dialog.Panel>
-                  </Transition.Child>
+          <StyledDialog
+            isOpen={isOpen}
+            onClose={closeDeleteModal}
+            title="Delete my account"
+            content={
+              <>
+                <div className="mt-2">
+                  <p className="text-sm text-red-500">
+                    Are you sure you want to&nbsp;
+                    <span className="font-bold">delete</span> your account?
+                  </p>
                 </div>
-              </div>
-            </Dialog>
-          </Transition>
+                <div className="flex justify-between mt-6 space-x-3">
+                  <Button
+                    variant={ButtonVariant.SECONDARY}
+                    onClick={closeDeleteModal}
+                    className="uppercase"
+                  >
+                    Cancel
+                  </Button>
+                  <IconButton
+                    variant={IconButtonVariant.DANGER}
+                    onClick={handleOnAccountDelete}
+                    icon={<TrashIcon className="w-5 h-5" />}
+                    className="uppercase"
+                  >
+                    Delete my account
+                  </IconButton>
+                </div>
+              </>
+            }
+          />
 
           <div className="flex w-full md:ml-2 md:w-auto">
             <IconButton
---
+++ src/shared/components/StyledDialog/StyledDialog.tsx
@@ -0,0 +1,58 @@
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
+interface StyledDialogProps {
+  title: string;
+  content: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export default function StyledDialog({
+  title,
+  content,
+  isOpen,
+  onClose,
+}: StyledDialogProps) {
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-zinc-900 bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="overflow-y-auto fixed inset-0">
+          <div className="flex justify-center items-center p-4 min-h-full text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="flex overflow-hidden flex-col justify-center p-6 w-auto max-w-md text-left bg-white rounded-md shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-zinc-900"
+                >
+                  {title}
+                </Dialog.Title>
+                {content}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
