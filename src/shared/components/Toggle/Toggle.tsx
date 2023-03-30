e1325df7328d77ffd3883a304dd14cdd05ddd2ab
Ryczko <konradryczko1@gmail.com>
2023-03-30 18:57:22 +0200

    Feature/Add toggle component

---
+++ src/shared/components/Toggle/Toggle.tsx
@@ -0,0 +1,34 @@
+import { Switch } from '@headlessui/react';
+import clsx from 'clsx';
+
+interface ToggleProps {
+  isEnabled: boolean;
+  onToggle: (isEnabled: boolean) => void;
+  label?: string;
+  classNames?: string;
+}
+
+function Toggle({ classNames, isEnabled, onToggle, label }: ToggleProps) {
+  return (
+    <Switch.Group>
+      <div className={clsx('flex items-center', classNames)}>
+        <Switch
+          checked={isEnabled}
+          onChange={onToggle}
+          className={`${
+            isEnabled ? 'bg-indigo-300' : 'bg-zinc-300'
+          } relative inline-flex h-6 w-11 items-center rounded-full transition-colors`}
+        >
+          <span
+            className={`${
+              isEnabled ? 'translate-x-6' : 'translate-x-1'
+            } inline-block h-4 w-4 transform rounded-full bg-white transition-transform`}
+          />
+        </Switch>
+        <Switch.Label className="ml-2">{label}</Switch.Label>
+      </div>
+    </Switch.Group>
+  );
+}
+
+export default Toggle;
