6d3adef2b429c7af39d005d455586c689431d820
Ryczko <konradryczko1@gmail.com>
2023-04-02 12:35:47 +0200

    Fix date formatting

--- src/features/surveys/components/DataCard/DataCard.tsx
+++ src/features/surveys/components/DataCard/DataCard.tsx
@@ -5,7 +5,7 @@ interface DataCardProps {
 
 export default function DataCard({ title, value }: DataCardProps) {
   return (
-    <div className="m-1 w-full rounded-lg border border-zinc-200 bg-white py-2 px-6 shadow-md sm:w-1/2 md:w-[200px]">
+    <div className="m-1 w-full rounded-lg border border-zinc-200 bg-white py-2 px-4 shadow-md sm:w-1/2 md:w-[220px]">
       <h3 className="mb-1 font-semibold">{title}</h3>
       {value}
     </div>
