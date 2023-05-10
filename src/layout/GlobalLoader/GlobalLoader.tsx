3a244dfb76b800a354d1f49a674beaafafad3e88
Ryczko <konradryczko1@gmail.com>
2023-05-10 18:14:37 +0200

    Fix global loader

--- src/layout/GlobalLoader/GlobalLoader.tsx
+++ src/layout/GlobalLoader/GlobalLoader.tsx
@@ -12,8 +12,15 @@ export default function GlobalLoader() {
       className="fixed left-0 top-0 z-50 h-screen w-screen bg-zinc-100"
     >
       <div className="fixed left-[50%] top-[45%] w-full translate-x-[-50%] translate-y-[-50%] text-center">
-        <Image priority src={logo} alt="logo" width={260} height={60} />
-        <Loader className="mt-1" isLoading />
+        <Image
+          className="mx-auto"
+          priority
+          src={logo}
+          alt="logo"
+          width={260}
+          height={60}
+        />
+        <Loader className="mt-3" isLoading />
       </div>
     </motion.div>
   );
