a8cb47deed3fded70d56e1f7cc968cb00c83dd3a
Ryczko <konradryczko1@gmail.com>
2023-05-26 16:20:09 +0200

    Update .env.example

--- src/features/authorization/managers/registerManager.ts
+++ src/features/authorization/managers/registerManager.ts
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { FormikHelpers } from 'formik';
 import * as Yup from 'yup';
 import useTranslation from 'next-translate/useTranslation';
-import { useRouter } from 'next/router';
 import { signIn } from 'next-auth/react';
 import { postFetch } from '../../../../lib/axiosConfig';
 import { isAxiosError } from 'axios';
