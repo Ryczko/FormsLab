5ff5f49ed8e4ebe7716d0c4d1248212505c7ca34
Jenish Dhanani <99007146+Jenish-Dhanani@users.noreply.github.com>
2023-08-22 20:04:05 +0530

    Feature/Thank you page
    Co-authored-by: jenish.dhanani <jenish.dhanani@tensorgo.com>

---
+++ cypress/e2e/thankyouPage.cy.ts
@@ -0,0 +1,45 @@
+import { faker } from '@faker-js/faker';
+
+describe('Create and Answer Survey', () => {
+  it('should redirect to login page when user is not logged in', () => {
+    cy.visit('/survey/create');
+    cy.url().should('include', '/login');
+  });
+
+  it('should create a survey and answer it and show thank you page', () => {
+    // Login before proceeding
+    cy.login();
+
+    // Create Survey
+    const surveyTitle = faker.lorem.sentence();
+    const questionContent = faker.lorem.sentence();
+
+    cy.get('[data-test-id="create-survey"]').click();
+    cy.url().should('include', '/survey/create');
+    cy.get('input[name="survey-title"]').type(surveyTitle);
+
+    cy.get('input[data-test-id="question-input-0"]').type(questionContent);
+    cy.get('input[data-test-id="question-input-1"]').type(questionContent);
+
+    cy.get('button[name="create-survey"]').click();
+
+    // Extract and visit the Answer Survey URL
+    cy.url().should('include', '/survey/answer/');
+    cy.url().then((currentUrl) => {
+      const urlParts = currentUrl.split('/');
+      const domain = urlParts.slice(0, 3).join('/');
+      const surveyId = urlParts.pop();
+      const newUrl = `${domain}/survey/${surveyId}`;
+
+      cy.visit(newUrl);
+
+      // Answer Survey
+      cy.get('button:has(em-emoji[shortcodes=":smiley:"])').should('be.visible').should('be.enabled').click();
+      cy.get('input[placeholder="Answer..."]').should('be.visible').type('test');
+      cy.contains('button', 'Send').should('be.visible').click();
+
+      // Verify Thank You Page
+      cy.url().should('include', '/thank-you');
+    });
+  });
+});
---
+++ src/pages/survey/[surveyId]/thank-you.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import useTranslation from 'next-translate/useTranslation';
+import Image from 'next/image';
+
+const ThankyouPage = () => {
+  const { t } = useTranslation('thankyou');
+
+  return (
+    <>
+      <Head>
+        <title>{t('title')}</title>
+        <meta name="description" content={t('content')} />
+      </Head>
+      <div className="pt-2">
+        <div className='flex justify-center'>
+          <Image src="/images/thankyou.svg" alt="thankyou" height="200" width="200" />
+        </div>
+        <h1 className="leading-tighter mb-4 pt-3 text-4xl font-extrabold tracking-tighter md:text-6xl">
+          {t('firstPartHeading')}&nbsp;
+          <span className="text-indigo-200">
+            {t('secondPartHeading')}
+          </span>
+        </h1>
+        <p className="mx-auto mb-6 mt-4 max-w-lg text-xl text-zinc-600">
+          {t('content')}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ThankyouPage;
