5db87c0727b31f7ed0260848085686d892e325fd
Ryczko <konradryczko1@gmail.com>
2023-09-22 22:16:36 +0200

    Update Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: 'cypress/support/e2e.ts',
-    videoUploadOnPasses: false,
     video: false,
     screenshotOnRunFailure: false,
     defaultCommandTimeout: 50000,
