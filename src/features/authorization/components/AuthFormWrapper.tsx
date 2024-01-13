909974dde74f02f2a14f6ff3d279cf7c899d4514
Ryczko <konradryczko1@gmail.com>
2024-01-13 20:35:48 +0100

    Fix auth wrapper

--- src/features/authorization/components/AuthFormWrapper.tsx
+++ src/features/authorization/components/AuthFormWrapper.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from 'react';
 export default function AuthFormWrapper({ children }: PropsWithChildren) {
   return (
     <section className="mx-auto flex flex-col items-center justify-center lg:mt-6">
-      <div className="mt-4 w-full rounded-lg border bg-white/30 p-6 shadow-sm sm:max-w-md">
+      <div className="mt-4 w-full rounded-lg border bg-white/50 p-6 shadow-sm sm:max-w-md">
         {children}
       </div>
     </section>
