df78aad655a3ca3deaa96f7f0c2601a193de3989
Shahzaib-Iqbal-1057 <shahzaibiqbal1057@gmail.com>
2024-01-17 01:07:07 +0500

    made custom svg implementation

--- src/shared/components/QuestionTypeIcons/EmojiIcon.tsx
+++ src/shared/components/QuestionTypeIcons/EmojiIcon.tsx
@@ -1,4 +1,3 @@
-import { EmojiHappyIcon } from '@heroicons/react/outline';
 import React from 'react';
 
 interface EmojiIconProps {
@@ -6,7 +5,25 @@ interface EmojiIconProps {
 }
 
 const EmojiIcon: React.FC<EmojiIconProps> = ({ 'data-test-id': testId }) => {
-  return <EmojiHappyIcon data-test-id={testId} />;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      data-test-id={testId}
+    >
+      <circle cx="12" cy="12" r="9" />
+      <path d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+      <line x1="9" y1="10" x2="9.01" y2="10" />
+      <line x1="15" y1="10" x2="15.01" y2="10" />
+    </svg>
+  );
 };
 
 export default EmojiIcon;
