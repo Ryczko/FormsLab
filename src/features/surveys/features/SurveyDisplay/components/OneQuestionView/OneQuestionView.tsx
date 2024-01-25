2a6888660610d2a7d1ae363e13238437e8180357
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-01-25 17:29:37 -0300

    hiding the progress bar

--- src/features/surveys/features/SurveyDisplay/components/OneQuestionView/OneQuestionView.tsx
+++ src/features/surveys/features/SurveyDisplay/components/OneQuestionView/OneQuestionView.tsx
@@ -3,10 +3,13 @@ import Header from 'shared/components/Header/Header';
 import Progressbar from 'shared/components/ProgressBar/ProgressBar';
 import { AnswersComponentFactory } from 'features/surveys/features/SurveyDisplay/components/AnswersComponent/AnswersComponentFactory';
 import { useSurveyDisplayContext } from 'features/surveys/features/SurveyDisplay/context';
+import { useSurveyCreatorContext } from 'features/surveys/features/SurveyCreator/managers/createSurveyManager/context';
 
 export default function OneQuestionView() {
   const { isAnswering, formData, activeQuestionIndex } =
     useSurveyDisplayContext();
+
+    const { surveyOptions } = useSurveyCreatorContext();
 
   return (
     <>
@@ -17,11 +20,11 @@ export default function OneQuestionView() {
       {formData.questions.length > 0 && (
         <>
           <AnswersComponentFactory questionIndex={activeQuestionIndex} />
-          <Progressbar
+          {!surveyOptions.hideProgressBar ? <Progressbar
             currentStep={activeQuestionIndex + 1}
             totalSteps={formData?.questions.length}
             isSubmitted={isAnswering}
-          />
+          /> : <></>}
         </>
       )}
     </>
